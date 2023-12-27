7293c7127dad3253ed9685ce343dc9de50652c75
maruyama-taiyo <maruyama.taiyo@itfllc.co.jp>
2023-12-27 13:57:17 +0900

    ある程度のレイアウトができたので作業ログを出す

---
+++ app/components/sideBar/Sidebar.tsx
@@ -0,0 +1,24 @@
+import { List, ListItem, Divider, Box, ListItemButton } from "@mui/material";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <Box width={180}>
+      <List>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+        <Divider></Divider>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+        <Divider></Divider>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+        <ListItemButton>koko</ListItemButton>
+      </List>
+    </Box>
+  );
+};
+
+export default Sidebar;
