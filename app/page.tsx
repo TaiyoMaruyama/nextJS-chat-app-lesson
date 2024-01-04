b89b10cebbb8e0d68909831f150b354bc5d6558a
maruyama-taiyo <maruyama.taiyo@itfllc.co.jp>
2024-01-05 01:16:38 +0900

    ヘッダーを削除して簡単にレイアウトを調節

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { Box } from "@mui/material";
-import AppHeader from "./components/common/AppHeader";
 import Sidebar from "./components/sideBar/Sidebar";
 import ChatArea from "./components/chat/ChatArea";
 
 export default function Home() {
   return (
     <Box>
-      <AppHeader />
       <Box display="flex">
         <Sidebar />
         <ChatArea />
